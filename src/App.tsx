0cc45337b9993a9b65ccfd1e69f6f0fef1243d85
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-03-29 17:34:21 -0300

    Lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+
 function App() {
   return (
-    <div>
-      <a href="instagram.com" target="_blank">Instagram</a>
-    </div>
+    <BrowserRouter>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/rooms/new" element={<NewRoom/>} />
+          </Routes>
+        </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
